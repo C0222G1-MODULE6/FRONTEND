3cfc87d2d03a3d4e2da4f91c5a4fa45763980f51
khanhtran9x <khanh150795@gmail.com>
2022-08-07 22:37:56 +0700

    first commit

---
+++ frontend-codegym/src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './common-component/header/header.component';
+import { IndexComponent } from './common-component/index/index.component';
+import { FooterComponent } from './common-component/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { TicketListComponent } from './ticket/component/ticket-list/ticket-list.component';
+import { TicketCreateComponent } from './ticket/component/ticket-create/ticket-create.component';
+import { TicketEditComponent } from './ticket/component/ticket-edit/ticket-edit.component';
+import { FirebaseChatComponent } from './firebase-chat/component/firebase-chat.component';
+import { LoginComponent } from './authentication/component/login/login.component';
+import {AuthInterceptor} from './authentication/model/auth-interceptor';
+import { LogoutComponent } from './authentication/component/logout/logout.component';
+import { NotAuthorizedComponent } from './common-component/not-authorized/not-authorized.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { UploadImageComponent } from './upload-image/component/upload-image.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    FooterComponent,
+    TicketListComponent,
+    TicketCreateComponent,
+    TicketEditComponent,
+    FirebaseChatComponent,
+    LoginComponent,
+    LogoutComponent,
+    NotAuthorizedComponent,
+    UploadImageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 2000,
+      closeButton: true,
+      progressBar: true,
+      positionClass: 	'toast-top-right',
+    }),
+  ],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend-codegym/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+// tslint:disable-next-line:prefer-const
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBSUbIYq8Eeyj4PLEYTOzsx-6504viMFpk',
+    authDomain: 'c0222g1-codegym.firebaseapp.com',
+    databaseURL: 'https://c0222g1-codegym-default-rtdb.firebaseio.com',
+    projectId: 'c0222g1-codegym',
+    storageBucket: 'c0222g1-codegym.appspot.com',
+    messagingSenderId: '304675475646',
+    appId: '1:304675475646:web:3b9a366587a20cd1134eef',
+    measurementId: 'G-DZ70EGTRXM'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
